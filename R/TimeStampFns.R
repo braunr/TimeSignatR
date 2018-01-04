92c552c863d4193607cb03d34dc02b575f7f0320
Rosemary Braun <rbraun@northwestern.edu>
2018-01-04 15:57:05 -0600

    added dependencies and tidied script

--- R/TimeStampFns.R
+++ R/TimeStampFns.R
@@ -1,5 +1,5 @@
 #####################################################################
-# Initial attempt at TimeStamp algorithm
+# Script for TimeStamp algorithm
 # (c) Rosemary Braun <rbraun@northwestern.edu>
 ######################################################################
 
@@ -66,7 +66,6 @@ recalibrateExprs <- function(exprMat,subjectIDs){
 
 #=====================================================================
 # FNS FOR MODELING X&Y CLOCK COORDS
-# NB: erroneous error model!
 #=====================================================================
 
 trainTimeStamp <- function(expr,subjIDs,times,trainFrac=0.5,plot=FALSE,recalib=FALSE,...){
@@ -106,49 +105,30 @@ predTimeStamp <- function(timestamp,newx=NULL,s=timestamp$cv.fit$lambda.min){
 	}
 }
 
+#=====================================================================
+# FNS FOR CALCULATING PREDICTION ERROR MODULO 24
+#=====================================================================
 
+timeErr <- function(trueTimes,predTimes){
+	predTimes <- predTimes%%24
+	trueTimes <- trueTimes%%24
+	timeDiffs <- abs(predTimes-trueTimes)
+	timeDiffs <- pmin(timeDiffs,24-timeDiffs)
+	return(timeDiffs)
+}
 
+timeErrSigned <- function(trueTimes,predTimes){
+	predTimes <- predTimes%%24
+	trueTimes <- trueTimes%%24
+	timeDiffs <- predTimes-trueTimes
+	timeDiffs[which(timeDiffs>12)]<-timeDiffs[which(timeDiffs>12)]-24
+	timeDiffs[which(timeDiffs<(-12))]<-timeDiffs[which(timeDiffs<(-12))]+24	
+	return(timeDiffs)
+}
 
 #=====================================================================
 # FNS FOR PLOTTING PREDICTED CLOCK COORDS
 #=====================================================================
-#--- "CLOCK" PLOT ---
-clockplot <- function(trueHr,predHr,IDs=1,col=rainbow(25)[round(trueHr%%24)+1],noweb=F){
-	#opar <- par(mfrow=c(1,2),xpd=T)	
-	opar <- par(xpd=T,mar=c(2,1.5,3,1.5))	
-	on.exit(par(opar))
-	web <- function(rads){
-		a <- seq(0,2*pi,length=180)
-		sapply(rads,function(r){lines(r*sin(a),r*cos(a),col="grey80",lwd=0.5)})
-		hrs <- (0:23)*2*pi/24
-		r <- 1.05*max(rads)
-		sapply(hrs,function(a){segments(0,0,r*sin(a),r*cos(a),col="grey80",lwd=0.5)})
-	}			
-	rads <- as.numeric(factor(IDs))
-	radmax <- max(rads)
-	tx <- rads*sin(trueHr*2*pi/24)
-	ty <- rads*cos(trueHr*2*pi/24)
-	px <- rads*sin(predHr*2*pi/24)
-	py <- rads*cos(predHr*2*pi/24)
-	plot(tx,ty,type="n",axes=F,bg=col,xlab="",ylab="",main="True time (24h)",pch=21,xlim=c(-radmax,radmax),ylim=c(-radmax,radmax))
-	text(
-		sin((0:23)*2*pi/24)*(radmax*1.1),
-		cos((0:23)*2*pi/24)*(radmax*1.1),
-		labels=as.character(0:23)
-	)
-	if(!noweb){web(rads)}
-	points(tx,ty,bg=col,pch=21)
-	plot(px,py,type="n",axes=F,bg=col,xlab="",ylab="",main="Predicted time (24h)",pch=21,xlim=c(-radmax,radmax),ylim=c(-radmax,radmax))
-	text(
-		sin((0:23)*2*pi/24)*(radmax*1.1),
-		cos((0:23)*2*pi/24)*(radmax*1.1),
-		labels=as.character(0:23)
-	)
-	if(!noweb){web(rads)}
-	points(px,py,bg=col,pch=21)
-}
-
-#--- PRED PLOT ---
 
 errplot <- function(trueHr,predHr,col=1,pch=1,main="Time of Day (24h)"){
 	# plot predicted vs. true time of day , with grey bands at 2 & 4h MOE
@@ -169,31 +149,6 @@ errplot <- function(trueHr,predHr,col=1,pch=1,main="Time of Day (24h)"){
 	points(trueHr,predHr,col=col,pch=pch)
 }
 
-timeErr <- function(trueTimes,predTimes,plot=F,...){
-	if(plot){
-		errplot(trueTimes,predTimes,...)
-	}
-	predTimes <- predTimes%%24
-	trueTimes <- trueTimes%%24
-	timeDiffs <- abs(predTimes-trueTimes)
-	timeDiffs <- pmin(timeDiffs,24-timeDiffs)
-	return(timeDiffs)
-}
-
-timeErrSigned <- function(trueTimes,predTimes,plot=F,...){
-	if(plot){
-		errplot(trueTimes,predTimes,...)
-	}
-	predTimes <- predTimes%%24
-	trueTimes <- trueTimes%%24
-	timeDiffs <- predTimes-trueTimes
-	#timeDiffs <- pmin(timeDiffs,24-timeDiffs)
-	timeDiffs[which(timeDiffs>12)]<-timeDiffs[which(timeDiffs>12)]-24
-	timeDiffs[which(timeDiffs<(-12))]<-timeDiffs[which(timeDiffs<(-12))]+24	
-	return(timeDiffs)
-}
-
-
 
 tolplot <- function(trueHr,predHr,add=FALSE,col=1){
 	# plot % correct by tolerance, dropping lines at median, 90quantile, & 1sd 
@@ -202,8 +157,6 @@ tolplot <- function(trueHr,predHr,add=FALSE,col=1){
 	hrerr <- abs(predTimes-trueTimes)
 	hrerr <- hrerr[!is.na(hrerr)]
 	hrerr <- pmin(hrerr,24-hrerr)
-	#hrerr <- cbind(predHr-trueHr, predHr-trueHr+24, predHr-trueHr-24)
-	#hrerr <- apply(hrerr,1,function(z){z[which.min(abs(z))]})
 	hrsoff <- seq(0,12,length=49)
 	fracacc <- sapply(hrsoff,function(hrtol){
 		100*sum(abs(hrerr)>hrtol)/length(hrerr)
@@ -211,14 +164,10 @@ tolplot <- function(trueHr,predHr,add=FALSE,col=1){
 	if(!add){
 		col=1
 		plot(hrsoff,100-fracacc,xlim=c(0,12),
-			#main="% correct by tolerance", xlab="",ylab="")
 			main="Absolute error CDF", xlab="",ylab="")
 		mtext("correct to within (hrs)",side=1,line=2.2,cex=0.8)
 		mtext(paste("% correct (N = ",length(hrerr),")",sep=""),side=2,line=2.4,cex=0.8)
-		#abline(h=c(50,90,100),v=c(median(abs(hrerr)),sd(hrerr),quantile(abs(hrerr),0.9)),col="grey")
 		abline(h=c(50,80,100),v=c(median(abs(hrerr)),quantile(abs(hrerr),0.8)),col="grey")
-		#text(sd(hrerr),0,round(sd(hrerr),2),cex=0.7)
-		#text(quantile(abs(hrerr),0.9),0,round(quantile(abs(hrerr),0.9),2),cex=0.7)
 		asTime <- function(x){
 			hrs <- (x%%24)%/%1; 
 			min <- round(60*(x%%1)); 
@@ -236,34 +185,16 @@ tolplot <- function(trueHr,predHr,add=FALSE,col=1){
 	if(!add){
 		text(10,20, sprintf("nAUC=%.2f",auc),cex=1,font=2)
 	}
-	#invisible(hrerr)
 	invisible(auc)
 }
+
 
 predplot <- function(trueHr,predHr,col=1,pch=1,main="Time of Day (24h)",...){
 	# do both of the above -- set par(mfrow=c(2,1)) or (1,2) first!
 	opar <- par(xpd=F,mar=c(4,4,3,1))	
 	on.exit(par(opar))
-	out <- timeErr(trueHr,predHr,plot=T,col,pch,main) #errplot(trueHr,predHr,col,pch,main)
+	out <- timeErr(trueHr,predHr,plot=T,col,pch,main) 
 	tolplot(trueHr,predHr)
 	invisible(out)
 }
-
-#--- STARBURST "GLOW" PLOT ---
-glowPlot <- function(predX,predY,trueHr,rad=2,main=NULL){
-	xlim=rad*c(-1,1);ylim=rad*c(-1,1)
-	plot(predX,predY,xlim=xlim,ylim=ylim,main=main,
-		type="n",xlab="",ylab="",axes=F)
-	invisible(sapply(0:23,function(hr){
-		wedgecol=adjustcolor(rainbow(24)[hr+1],alpha=0.4)
-		polygon(
-			c(0,10*sin(hr*2*pi/24),10*sin((hr+1)*2*pi/24),0),
-			c(0,10*cos(hr*2*pi/24),10*cos((hr+1)*2*pi/24),0),
-			col=wedgecol,border=0
-		)
-	}))
-	lines(sin(seq(0,2*pi,length=180)),cos(seq(0,2*pi,length=180)))
-	points(predX,predY,pch=21,bg=rainbow(25)[round(trueHr%%24)+1])
-}
-
 
